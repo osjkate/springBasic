f4f765458c2255d24a2804289a200637c518603f
sujinoh <osjkate@sookmyung.ac.kr>
2023-02-01 02:33:08 +0900

    컴포넌트 스캔

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = "hello.core",
+//        basePackageClasses = AutoAppConfig.class,
+        // base를 지정하지 않으면, @ComponentScan 이 붙은 설정 정보 클래스의 패키지부터 scan하기 시작한다.
+        // 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것이 좋음
+        // 프로젝트 메인 설정 정보는 프로젝트를 대표하는 정보이기 때문에 프로젝트 시작 루트 위치에 두는 것이 좋다.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+    // 수동 빈 등록 충돌 예시
+    /*@Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    */
+}
